65031b5943d08ac667d0500a2a6e5200e1ed711f
unknown <hktbluesky@gmail.com>
2026-04-18 11:06:01 +0900

    add Version lapper for preprocess to get version from nbgv

---
+++ TextureAllocator/Version.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextureAllocator;
+
+internal static class VersionInfo
+{
+    public static readonly string CurrentVersion = ThisAssembly.AssemblyInformationalVersion.Split("+")[0];
+}
