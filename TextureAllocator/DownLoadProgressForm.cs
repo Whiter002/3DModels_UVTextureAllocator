b2e56826408df09f911de2723a7a4fd1de0d05ed
unknown <hktbluesky@gmail.com>
2026-04-19 15:22:52 +0900

    add download form

---
+++ TextureAllocator/DownLoadProgressForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TextureAllocator;
+
+public partial class DownLoadProgressForm : Form
+{
+    public DownLoadProgressForm()
+    {
+        InitializeComponent();
+    }
+}
